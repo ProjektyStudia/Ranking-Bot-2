d36f8a006e5396fdfb0e4d696b20f98329c46811
Wojciech Łuszczak <wojtekluszczakpl@gmail.com>
2022-10-13 14:36:48 +0200

    Comment unnecessay programm file

--- client.py
+++ client.py
@@ -1,47 +1,50 @@
-import os
-import random
+#it's second way to connect bot, we won't use it
+#instead we will use bot.py
 
-import discord
-intents = discord.Intents.default()
-intents.messages = True
-intents.message_content  = True
-from dotenv import load_dotenv
+# import os
+# import random
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+# import discord
+# intents = discord.Intents.default()
+# intents.messages = True
+# intents.message_content  = True
+# from dotenv import load_dotenv
 
-client = discord.Client(intents=intents)
+# load_dotenv()
+# TOKEN = os.getenv('DISCORD_TOKEN')
+# GUILD = os.getenv('DISCORD_GUILD')
 
-@client.event
-async def on_ready():
-    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+# client = discord.Client(intents=intents)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# @client.event
+# async def on_ready():
+#     guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+#     print(
+#         f'{client.user} is connected to the following guild:\n'
+#         f'{guild.name}(id: {guild.id})'
+#     )
 
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
 
-    print("Received a message:", message.content)
+#     brooklyn_99_quotes = [
+#         'I\'m the human form of the 💯 emoji.',
+#         'Bingpot!',
+#         (
+#             'Cool. Cool cool cool cool cool cool cool, '
+#             'no doubt no doubt no doubt no doubt.'
+#         ),
+#     ]
 
-    if message.content == '99!':
-        response = random.choice(brooklyn_99_quotes)
-        await message.channel.send(response)
+#     print("Received a message:", message.content)
 
-    if message.content == "ping":
-        await message.channel.send('pong')
+#     if message.content == '99!':
+#         response = random.choice(brooklyn_99_quotes)
+#         await message.channel.send(response)
 
-client.run(TOKEN)
+#     if message.content == "ping":
+#         await message.channel.send('pong')
+
+# client.run(TOKEN)
